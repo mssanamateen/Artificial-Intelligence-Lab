602e1d545c978175b0856b2092f289ee21ba49cd
mssanamateen <mssanamateen@gmail.com>
2024-09-10 21:36:24 +0530

    updated

--- waterjug code/waterjbfs.py
+++ waterjug code/waterjbfs.py
@@ -1,7 +1,7 @@
 import math
 from collections import deque
 
-# Input capacities and initial/final states for jugs
+''' Input capacities and initial/final states for jugs'''
 a = int(input("Enter Jug A Capacity: "))
 b = int(input("Enter Jug B Capacity: "))
 ai = int(input("Initially Water in Jug A: "))
@@ -9,14 +9,16 @@ bi = int(input("Initially Water in Jug B: "))
 af = int(input("Final State of Jug A: "))
 bf = int(input("Final State of Jug B: "))
 
-# Check for negative values
+# Check for negative values and whether initial state is equal to final state
 if a <= 0 or b <= 0:
     print("Jug capacities must be positive.")
     exit(1)
 if ai < 0 or bi < 0 or af < 0 or bf < 0:
     print("Negative values are not allowed.")
     exit(1)
-
+if ai==af and bi==bf:
+    print(f"initial state is already the final state: juga{ai} and jugb={bi}")
+    exit()
 # Define the water jug solver function using BFS
 def bfs_wjug(a, b, ai, bi, af, bf):
     visited = set()
@@ -54,11 +56,11 @@ def bfs_wjug(a, b, ai, bi, af, bf):
     print("No solution found.")
     return
 
-# Check if the final state is achievable using GCD
+# Check if the final state can be achievable using GCD
 gcd = math.gcd(a, b)
 
 if (af <= a and bf <= b) and (af % gcd == bf % gcd == 0):
     bfs_wjug(a, b, ai, bi, af, bf)
 else:
     print("The final state is not achievable with the given capacities.")
-    exit(1)
+    exit()
